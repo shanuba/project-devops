1a8fa0f7a5af6bc57618904a4d5f62ff27dd65ae
shanuba <shanuba537@gmail.com>
2025-04-30 14:21:45 +0300

    jenkins files included

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo"current branch:$Branchname"
-if["$branchname"=="dev-repo"];then
+echo"current branch:$BRANCH_NAME"
+if["$BRANCH_NAME"=="dev-repo"];then
 docker build -t shanuba/dev-repo-dockimg
 echo"Shanuba" | docker login -u "shanuba" --password-stdin 
 docker push shanuba/dev-repo-dockimg:latest
 
-elif["$branchname"=="prod-repo"];then
+elif["$BRANCH_NAME"=="prod-repo"];then
 docker build -t shanuba/prod-repo-dockimg
 echo"Shanuba" | docker login -u "shanuba" --password-stdin 
 docker push shanuba/prod-repo-dockimg:latest
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo"Deployment branch:$Branchname"
+echo"Deployment branch:$BRANCH_NAME"
 docker stop dockimg-container ||true
 docker rm dockimg-container ||true
 
-if["$branchname"=="dev-repo"];then
+if["$BRANCH_NAME"=="dev-repo"];then
 docker pull shanuba/dev-repo-dockimg:latest
 docker run -d -p 80:80--name dockimg-container shanuba/dev-repo-dockimg:latest 
-elseif["$branchname"=="prod-repo"];then
+elseif["$BRANCH_NAME"=="prod-repo"];then
 docker pull shanuba/prod-repo-dockimg:latest
 docker run -d -p 80:80--name dockimg-container shanuba/prod-repo-dockimg:latest 
 fi
