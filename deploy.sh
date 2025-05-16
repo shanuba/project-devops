8d85a1645b0e317cfe7fc6cb8b2096ca20a6c2a2
Shanuba-github <104367849+shanuba@users.noreply.github.com>
2025-05-16 15:01:45 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 
 # if["$BRANCH_NAME"=="dev-repo"];then
 docker pull shanuba/dev-repo
-docker run -idd -p 80:80 shanuba/dev-repo
+docker run -idd -p 90:80 shanuba/dev-repo
 #elseif["$BRANCH_NAME"=="prod-repo"];then
 #docker pull shanuba/prod-repo-dockimg:latest
 #docker run -d -p 80:80--name dockimg-container shanuba/prod-repo-dockimg:latest 
