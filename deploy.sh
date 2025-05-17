296af1b65378b711a14b9b3611708555167fe3c3
Shanuba-github <104367849+shanuba@users.noreply.github.com>
2025-05-17 09:25:01 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo"Deployment branch:$BRANCH_NAME"
-docker stop dockimg-container ||true
-docker rm dockimg-container ||true
+# echo"Deployment branch:$BRANCH_NAME"
+# docker stop dockimg-container ||true
+# docker rm dockimg-container ||true
 
-if["$BRANCH_NAME"=="dev"];then
-docker pull shanuba/dev-dockimg:latest
-docker run -d -p 80:80--name dockimg-container shanuba/dev-dockimg:latest 
-elseif["$BRANCH_NAME"=="prod"];then
-docker pull shanuba/prod-dockimg:latest
-docker run -d -p 80:80--name dockimg-container shanuba/prod-dockimg:latest 
-fi
+# if["$BRANCH_NAME"=="dev-repo"];then
+docker pull shanuba/dev-repo
+docker run -idd -p 90:80 shanuba/dev-repo
+#elseif["$BRANCH_NAME"=="prod-repo"];then
+#docker pull shanuba/prod-repo-dockimg:latest
+#docker run -d -p 80:80--name dockimg-container shanuba/prod-repo-dockimg:latest 
+#fi
