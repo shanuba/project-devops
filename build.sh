342fae80b3a64344acaf3ebb9dd71dc4ffc03ba4
Shanuba-github <104367849+shanuba@users.noreply.github.com>
2025-05-17 08:09:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-echo"current branch:$BRANCH_NAME"
-if["$BRANCH_NAME"=="dev"];then
-docker build -t shanuba/dev-dockimg
-echo"Shanuba" | docker login -u "shanuba" --password-stdin 
-docker push shanuba/dev-dockimg:latest
 
-elif["$BRANCH_NAME"=="prod"];then
-docker build -t shanuba/prod-dockimg
-echo"Shanuba" | docker login -u "shanuba" --password-stdin 
-docker push shanuba/prod-dockimg:latest
+echo "Current branch: $GIT_BRANCH"
+
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    docker build -t shanuba/dev-repo .
+    echo "Shanuba" | docker login -u shanuba -p dckr_pat_z0obnJEPyEqtJNQtxCNOzaig89U
+    docker push shanuba/dev-repo
+
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    docker build -t shanuba/prod-repo .
+    echo "Shanuba" | docker login -u shanuba -p dckr_pat_z0obnJEPyEqtJNQtxCNOzaig89U
+    docker push shanuba/prod-repo
 fi
+ 
